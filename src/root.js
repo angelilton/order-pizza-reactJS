f48378af0c8b0dcfc45294a2d37ac554bc9522a9
angelilton <epifanio.rds@gmail.com>
2020-12-16 21:04:58 +0000

    add theme to global

--- src/root.js
+++ src/root.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { CssBaseline } from '@material-ui/core'
+import { CssBaseline, createMuiTheme } from '@material-ui/core'
+import { ThemeProvider } from 'styled-components'
 import AuthProvider from 'contexts/auth'
 import App from './app'
 
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true
+  }
+})
+
+console.log('theme:', theme)
+
 function Root() {
   return (
-    <AuthProvider>
-      <CssBaseline />
-      <Router>
-        <Route component={App} />
-      </Router>
-    </AuthProvider>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <CssBaseline />
+        <Router>
+          <Route component={App} />
+        </Router>
+      </AuthProvider>
+    </ThemeProvider>
   )
 }
 export default hot(Root)
