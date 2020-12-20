5acbf61deed0a0bdd8e347e806937aa30955f8d9
angelilton <epifanio.rds@gmail.com>
2020-12-20 20:08:17 +0000

    add ChooseQuantity.js

---
+++ src/pages/choose-pizza-quantity/choose-quantity.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const chooseQuantity = () => {
+  console.log('page quantity on')
+  return (
+    <div>
+      <h1>quantas pizza vai ser</h1>
+    </div>
+  )
+}
+
+export default chooseQuantity
