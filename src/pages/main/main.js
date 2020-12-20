70086598582d78c6df094af0f21856ac56abc56b
angelilton <epifanio.rds@gmail.com>
2020-12-20 20:08:39 +0000

    added router quantity to main.js

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -2,12 +2,15 @@ import React, { Suspense } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Header from './header'
 
-import { HOME, CHOOSE_PIZZA_FLAVOURS } from 'routes'
+import { HOME, CHOOSE_PIZZA_FLAVOURS, CHOOSE_QUANTITY } from 'routes'
 
 const ChoosePizzaSize = React.lazy(() => import('pages/choose-pizza-size'))
+
 const ChoosePizzaFlavours = React.lazy(() =>
   import('pages/choose-pizza-flavours')
 )
+
+const ChooseQuantity = React.lazy(() => import('pages/choose-pizza-quantity'))
 
 const Main = () => (
   <>
@@ -15,11 +18,8 @@ const Main = () => (
     <Suspense fallback="Loading...">
       <Switch>
         <Route path={HOME} exact component={ChoosePizzaSize} />
-        <Route
-          path={CHOOSE_PIZZA_FLAVOURS}
-          exact
-          component={ChoosePizzaFlavours}
-        />
+        <Route path={CHOOSE_PIZZA_FLAVOURS} component={ChoosePizzaFlavours} />
+        <Route path={CHOOSE_QUANTITY} component={ChooseQuantity} />
       </Switch>
     </Suspense>
   </>
