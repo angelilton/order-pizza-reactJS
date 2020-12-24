6738b1ed013fe80cdf206c1203fac1a2141aa24d
angelilton <epifanio.rds@gmail.com>
2020-12-24 14:27:08 +0000

    add pizzas to orderContext

--- src/contexts/order.js
+++ src/contexts/order.js
@@ -1,10 +1,26 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 import PropTypes from 'prop-types'
 
 export const OrderContext = createContext()
 
 const Order = ({ children }) => {
-  return <OrderContext.Provider value={{}}>{children}</OrderContext.Provider>
+  const [pizzas, setPizzas] = useState([])
+
+  function addPizzaToOrder(pizza) {
+    setPizzas([...pizzas, pizza])
+  }
+  return (
+    <OrderContext.Provider
+      value={{
+        order: {
+          pizzas
+        },
+        addPizzaToOrder
+      }}
+    >
+      {children}
+    </OrderContext.Provider>
+  )
 }
 
 Order.propTypes = {
