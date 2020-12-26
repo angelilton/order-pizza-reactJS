ecfeb6047f50d41447d08b7f46425eb620c8afce
angelilton <epifanio.rds@gmail.com>
2020-12-26 15:14:33 +0000

    add footerCheckout to checkout.js & add new route

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {
+  Button,
   Grid,
   List,
   ListItem,
@@ -8,67 +9,84 @@ import {
   TextField as MaterialTextField,
   Typography
 } from '@material-ui/core'
-import { Title as UiTitle, Wrapper } from 'ui'
+import { FooterCheckout, Title as UiTitle, Wrapper } from 'ui'
 import styled, { css } from 'styled-components'
 import { useOrder } from 'hooks'
 import { singularOrPlural } from 'utils'
+import { Link } from 'react-router-dom'
+import { CHECKOUT_CONFIRMATION } from 'routes'
 
 const Checkout = () => {
   const { order } = useOrder()
   console.log('order:', order)
 
   return (
-    <Wrapper>
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={6}>
-          <Title>what is your address for delivery?</Title>
-          <PaperWrapper>
-            <Grid container spacing={2}>
-              <TextField label="CEP" xs={4} autoFocus />
-              <Grid item xs={6} />
-              <TextField label="Rua" xs={9} />
-              <TextField label="Número" xs={3} />
-              <TextField label="Complemento" xs={12} />
-              <TextField label="Cidade" xs={8} />
-              <TextField label="Estado" xs={4} />
-            </Grid>
-          </PaperWrapper>
+    <>
+      <Wrapper>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={6}>
+            <Title>what is your address for delivery?</Title>
+            <PaperWrapper>
+              <Grid container spacing={2}>
+                <TextField label="CEP" xs={4} autoFocus />
+                <Grid item xs={6} />
+                <TextField label="Rua" xs={9} />
+                <TextField label="Número" xs={3} />
+                <TextField label="Complemento" xs={12} />
+                <TextField label="Cidade" xs={8} />
+                <TextField label="Estado" xs={4} />
+              </Grid>
+            </PaperWrapper>
 
-          <Title>what is your phone number?</Title>
-          <PaperWrapper>
-            <TextField label="Telefone" xs={6} />
-          </PaperWrapper>
+            <Title>what is your phone number?</Title>
+            <PaperWrapper>
+              <TextField label="Telefone" xs={6} />
+            </PaperWrapper>
+          </Grid>
+
+          <Grid container direction="column" item xs={12} md={6}>
+            <Title>Your order:</Title>
+            <PaperWrapper>
+              <List>
+                {order.pizzas.map((pizza, index) => {
+                  const { pizzaFlavours, pizzaSize, quantity } = pizza
+
+                  return (
+                    <ListItem key={index}>
+                      <Typography>
+                        {quantity}
+                        {' - '} {singularOrPlural(quantity, 'pizza', 'pizzas')}{' '}
+                        <b>{pizzaSize.name.toUpperCase()}</b>
+                        <br />
+                        {singularOrPlural(
+                          pizzaFlavours.length,
+                          'Flavour: ',
+                          'Flavours: '
+                        )}{' '}
+                        <b>
+                          {pizzaFlavours.map(({ name }) => name).join(', ')}
+                        </b>
+                      </Typography>
+                    </ListItem>
+                  )
+                })}
+              </List>
+            </PaperWrapper>
+          </Grid>
         </Grid>
+      </Wrapper>
 
-        <Grid container direction="column" item xs={12} md={6}>
-          <Title>Your order:</Title>
-          <PaperWrapper>
-            <List>
-              {order.pizzas.map((pizza, index) => {
-                const { pizzaFlavours, pizzaSize, quantity } = pizza
-
-                return (
-                  <ListItem key={index}>
-                    <Typography>
-                      {quantity}
-                      {' - '} {singularOrPlural(quantity, 'pizza', 'pizzas')}{' '}
-                      <b>{pizzaSize.name.toUpperCase()}</b>
-                      <br />
-                      {singularOrPlural(
-                        pizzaFlavours.length,
-                        'Flavour: ',
-                        'Flavours: '
-                      )}{' '}
-                      <b>{pizzaFlavours.map(({ name }) => name).join(', ')}</b>
-                    </Typography>
-                  </ListItem>
-                )
-              })}
-            </List>
-          </PaperWrapper>
-        </Grid>
-      </Grid>
-    </Wrapper>
+      <FooterCheckout>
+        <Button
+          variant="contained"
+          color="primary"
+          component={Link}
+          to={CHECKOUT_CONFIRMATION}
+        >
+          Order
+        </Button>
+      </FooterCheckout>
+    </>
   )
 }
 
--- src/routes.js
+++ src/routes.js
@@ -3,3 +3,4 @@ export const LOGIN = '/login'
 export const CHOOSE_PIZZA_FLAVOURS = '/pizza-flavours'
 export const CHOOSE_QUANTITY = '/choose-quantity'
 export const CHECKOUT = '/checkout'
+export const CHECKOUT_CONFIRMATION = `${CHECKOUT}/confirmation`
