368ac2a7ecdbb53f17c5536348f0a266a114983b
angelilton <epifanio.rds@gmail.com>
2021-01-03 14:08:07 +0000

    add removePizzafromorder & redirect if order is 0

--- src/contexts/order.js
+++ src/contexts/order.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from 'react'
 import PropTypes from 'prop-types'
+import { uniqueID } from 'utils'
 
 export const OrderContext = createContext()
 
@@ -7,15 +8,29 @@ const Order = ({ children }) => {
   const [pizzas, setPizzas] = useState([])
 
   function addPizzaToOrder(pizza) {
-    setPizzas([...pizzas, pizza])
+    setPizzas((pizzas) => pizzas.concat(newPizza(pizza)))
   }
+
+  function newPizza(pizza) {
+    return {
+      id: uniqueID(),
+      ...pizza
+    }
+  }
+
+  function removePizzaFromOrder(id) {
+    console.log('removePizzaFromOrder:', id)
+    setPizzas((pizzas) => pizzas.filter((p) => p.id !== id))
+  }
+
   return (
     <OrderContext.Provider
       value={{
         order: {
           pizzas
         },
-        addPizzaToOrder
+        addPizzaToOrder,
+        removePizzaFromOrder
       }}
     >
       {children}
--- src/pages/checkout/order-info.js
+++ src/pages/checkout/order-info.js
@@ -1,18 +1,33 @@
 import React from 'react'
-import { List, ListItem, Typography } from '@material-ui/core'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import {
+  IconButton,
+  List,
+  ListItem as MListItem,
+  Typography
+} from '@material-ui/core'
+import { Close } from '@material-ui/icons'
+
 import { singularOrPlural } from 'utils'
 import { useOrder } from 'hooks'
+import { Redirect } from 'react-router-dom'
+import { HOME } from 'routes'
 
-const OrderInfo = () => {
-  const { order } = useOrder()
+const OrderInfo = ({ showOptions }) => {
+  const { order, removePizzaFromOrder } = useOrder()
+
+  if (!order.pizzas.length) {
+    return <Redirect to={HOME} />
+  }
 
   return (
     <List>
-      {order.pizzas.map((pizza, index) => {
+      {order.pizzas.map((pizza) => {
         const { pizzaFlavours, pizzaSize, quantity } = pizza
 
         return (
-          <ListItem key={index}>
+          <ListItem key={pizza.id}>
             <Typography>
               {quantity}
               {' - '} {singularOrPlural(quantity, 'pizza', 'pizzas')}{' '}
@@ -25,11 +40,29 @@ const OrderInfo = () => {
               )}{' '}
               <b>{pizzaFlavours.map(({ name }) => name).join(', ')}</b>
             </Typography>
+            {showOptions && (
+              <IconButton
+                title="Remove"
+                color="secondary"
+                onClick={() => removePizzaFromOrder(pizza.id)}
+              >
+                <Close />
+              </IconButton>
+            )}
           </ListItem>
         )
       })}
     </List>
   )
 }
+
+OrderInfo.propTypes = {
+  showOptions: PropTypes.bool
+}
+
+const ListItem = styled(MListItem)`
+  display: flex;
+  justify-content: space-between;
+`
 
 export default OrderInfo
